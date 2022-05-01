2497f76ce109f17ef044fb961638611498f3634a
Luiz Ribeiro <luizribeiro@gmail.com>
2022-04-30 19:55:56 -0500

    Support for parsing image name components

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,16 @@
 use dkregistry::v2::Client;
+use regex::Regex;
+
+fn get_image_components(raw_image: &str) -> (&str, &str, &str) {
+    let re = Regex::new(r"(?:([a-z0-9.-]+)/)?([a-z-]+/[a-z-]+):?([a-z0-9.-]+)?").unwrap();
+    let caps = re.captures(raw_image).unwrap();
+
+    let registry = caps.get(1).map_or("registry-1.docker.io", |m| m.as_str());
+    let image = caps.get(2).map(|m| m.as_str()).unwrap();
+    let tag = caps.get(3).map_or("latest", |m| m.as_str());
+
+    return (registry, image, tag);
+}
 
 async fn get_digest<'a>(
     registry: &'a str,
@@ -13,6 +25,10 @@ async fn get_digest<'a>(
 
 #[tokio::main]
 async fn main() {
-    let digest = get_digest("ghcr.io", "home-assistant/home-assistant", "stable").await.unwrap();
+    let (registry, image, tag) = get_image_components("acockburn/appdaemon:4.2.1");
+    println!("registry: {}", registry);
+    println!("image: {}", image);
+    println!("tag: {}", tag);
+    let digest = get_digest(registry, image, tag).await.unwrap();
     println!("digest: {}", digest);
 }
