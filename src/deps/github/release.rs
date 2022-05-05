0d11a57de1c72ddd387d8d574c944d513041c763
Luiz Ribeiro <luizribeiro@gmail.com>
2022-05-05 16:39:11 -0500

    Support for locking GitHub releases

--- src/deps/github/release.rs
+++ src/deps/github/release.rs
@@ -9,6 +9,9 @@ use serde::{Deserialize, Serialize};
 pub struct GitHubRelease {
     owner: String,
     repo: String,
+    override_scheme: Option<String>,
+    override_domain: Option<String>,
+    override_nix_sha256: Option<String>,
 }
 
 impl GitHubRelease {
@@ -18,6 +21,39 @@ impl GitHubRelease {
             _ => Err("Error while parsing arguments of uptix.githubRelease"),
         }
     }
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+struct GitHubLatestReleaseInfo {
+    tag_name: String,
+}
+
+async fn fetch_github_latest_release(dependency: &GitHubRelease) -> GitHubLatestReleaseInfo {
+    let client = reqwest::Client::new();
+    let url_as_str = format!(
+        "{}://{}/repos/{}/{}/releases/latest",
+        dependency
+            .override_scheme
+            .as_ref()
+            .unwrap_or(&"https".to_string()),
+        dependency
+            .override_domain
+            .as_ref()
+            .unwrap_or(&"api.github.com".to_string()),
+        dependency.owner,
+        dependency.repo,
+    );
+    let url = reqwest::Url::parse(&url_as_str).unwrap();
+    let response = client
+        .request(reqwest::Method::GET, url)
+        .header(reqwest::header::USER_AGENT, util::user_agent())
+        .send()
+        .await
+        .unwrap()
+        .text()
+        .await
+        .unwrap();
+    return serde_json::from_str(&response).unwrap();
 }
 
 #[async_trait]
@@ -30,11 +66,16 @@ impl Lockable for GitHubRelease {
     }
 
     async fn lock(&self) -> Result<Box<dyn erased_serde::Serialize>, &'static str> {
+        let rev = fetch_github_latest_release(self).await.tag_name;
+        let sha256 = match &self.override_nix_sha256 {
+            Some(s) => s.to_string(),
+            None => github::compute_nix_sha256(&self.owner, &self.repo, &rev),
+        };
         return Ok(Box::new(github::GitHubLock {
             owner: self.owner.clone(),
             repo: self.repo.clone(),
-            rev: "TODO".to_string(),
-            sha256: "TODO".to_string(),
+            rev,
+            sha256,
         }));
     }
 }
@@ -44,6 +85,7 @@ mod tests {
     use super::GitHubRelease;
     use crate::deps::collect_ast_dependencies;
     use crate::deps::Lockable;
+    use serde_json::json;
 
     #[test]
     fn it_parses() {
@@ -79,5 +121,42 @@ mod tests {
 
     #[tokio::test]
     async fn it_locks() {
+        let address = mockito::server_address().to_string();
+        let _latest_release_mock = mockito::mock("GET", "/repos/luizribeiro/uptix/releases/latest")
+            .match_header(
+                &reqwest::header::USER_AGENT.to_string(),
+                mockito::Matcher::Regex(r"^uptix/[0-9.]+$".to_string()),
+            )
+            .with_status(200)
+            .with_body(
+                r#"{
+                    "tag_name": "v0.1.0"
+                }"#,
+            )
+            .create();
+
+        let dependency = GitHubRelease {
+            owner: "luizribeiro".to_string(),
+            repo: "uptix".to_string(),
+            override_scheme: Some("http".to_string()),
+            override_domain: Some(address),
+            override_nix_sha256: Some(
+                "1vxzg4wdjvfnc7fjqr9flza5y7gh69w0bpf7mhyf06ddcvq3p00j".to_string(),
+            ),
+        };
+        let lock = dependency.lock().await.unwrap();
+        let lock_value = serde_json::to_value(lock).unwrap();
+
+        assert_eq!(
+            lock_value,
+            json!({
+                "owner": "luizribeiro",
+                "repo": "uptix",
+                "rev": "v0.1.0",
+                "sha256": "1vxzg4wdjvfnc7fjqr9flza5y7gh69w0bpf7mhyf06ddcvq3p00j",
+            }),
+        );
+
+        mockito::reset();
     }
 }
