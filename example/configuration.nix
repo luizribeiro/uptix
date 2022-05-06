c2b788bdb5672257154fe8b96e044dd76a3da9c0
Luiz Ribeiro <luizribeiro@gmail.com>
2022-05-06 11:50:26 -0500

    Added comments to example/configuration.nix

--- example/configuration.nix
+++ example/configuration.nix
@@ -1,6 +1,8 @@
 { uptix, pkgs, ... }:
 
 let
+  # this is a rust package which is being built from the latest commit on
+  # the main branch of the luizribeiro/hello-world-rs github repo
   helloWorldRS = pkgs.rustPlatform.buildRustPackage {
     pname = "hello-world-rs";
     version = "0.1.0";
@@ -11,6 +13,8 @@ let
     });
     cargoSha256 = "sha256-p6vLLM6A16o8dKLwUfP/qf4crnzlgp4f+Vs0ocRChE4=";
   };
+  # this is the same rust package as above, but being built from the latest
+  # released version on that same github repo
   releasedHelloWorldRS =
     let
       release = uptix.githubRelease {
@@ -30,6 +34,8 @@ in
 
   virtualisation.oci-containers.containers = {
     homeassistant = {
+      # this container is using the latest released docker image under the
+      # stable tag
       image = uptix.dockerImage "homeassistant/home-assistant:stable";
     };
   };
