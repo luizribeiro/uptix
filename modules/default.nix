ef9f6525ef9c8f7948c4422d4a3a4d3924790172
Luiz Ribeiro <luizribeiro@gmail.com>
2022-05-05 21:30:26 -0500

    Added uptix.version utility

--- modules/default.nix
+++ modules/default.nix
@@ -1,8 +1,12 @@
 { lockFile }:
 
+with builtins;
+
 let
-  importJSON = path: builtins.fromJSON (builtins.readFile path);
   lockFor = key: (importJSON lockFile).${key};
+  # from nixpkgs.lib
+  importJSON = path: fromJSON (readFile path);
+  hasPrefix = pref: str: substring 0 (stringLength pref) str == pref;
 in
 {
   dockerImage = name: "${name}@${lockFor name}";
@@ -10,4 +14,9 @@ in
     (lockFor "$GITHUB_BRANCH$:${owner}/${repo}:${branch}");
   githubRelease = { owner, repo }:
     (lockFor "$GITHUB_RELEASE$:${owner}/${repo}");
+  version = githubRelease:
+    let rev = githubRelease.rev; in
+    if hasPrefix "v" rev
+    then (substring 1 (stringLength rev) rev)
+    else rev;
 }
